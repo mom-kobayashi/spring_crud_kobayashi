636b1c83604a60fb078b15a5def1c250a6d5876f
mom-kobayashi <mom-kobayashi@3sss.co.jp>
2026-06-02 22:37:04 +0900

    spring_crud_kobayashi最新版

--- src/main/java/jp/co/sss/crud/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package jp.co.sss.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import jp.co.sss.crud.entity.Department;
-
-public interface DepartmentRepository extends JpaRepository<Department, Integer>{
-	
-}
