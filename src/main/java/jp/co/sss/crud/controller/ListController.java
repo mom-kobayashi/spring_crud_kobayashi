f365545062caab0eb5d1ed958c13be8182b6e02b
mom-kobayashi <mom-kobayashi@3sss.co.jp>
2026-06-03 18:21:21 +0900

    同じファイル編集

--- src/main/java/jp/co/sss/crud/controller/ListController.java
+++ src/main/java/jp/co/sss/crud/controller/ListController.java
@@ -1,6 +1,6 @@
 //同じファイル編集、pullしてコミット
 package jp.co.sss.crud.controller;
-
+// 同じファイル編集
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
